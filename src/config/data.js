b6ba0a80338eed824005b2e423fe99ecfc183930
athav2823 <atharvaneware@gmail.com>
2024-08-20 13:55:45 +0530

    configurable ui is ready

---
+++ src/config/data.js
@@ -0,0 +1,81 @@
+const MAIN_TITLE = "भोजन भंडार"
+
+
+
+const PRODUCTS = [
+    {
+        title : "Chiken Biryani",
+        description :"“Spice up your life with some Chicken Biryani magic! ✨”",
+        price : "₹ 150",
+        imgUrl:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Fhandi-chicken-biryani.html&psig=AOvVaw25QzZu7Exdlgz1OXVTr7Ff&ust=1723989428099000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLCPxbOX_IcDFQAAAAAdAAAAABAE",
+        isveg : false
+        },
+        {
+           title : "Pizza",
+           description :"“A slice a day keeps the sadness away 🍕😊”",
+           price : "₹ 250",
+           imgUrl:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-ai-image%2Fpizza-with-cold-drink-table_79014166.htm&psig=AOvVaw3xp8KtJHL-4EY8_WUR6teo&ust=1724224719508000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwjy5c_gg4OIAxUPTWwGHUVqC78QjRx6BAgAEBg",
+           isveg : true
+        },
+        {
+           title : "Waradi Thali",
+           description :"“Savoring the rich flavors of Maharashtra with this Varadi Thali. 🌶️”",
+           price : "₹ 299",
+           imgUrl:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zomato.com%2Fpt%2Fncr%2Fthaal-vaadi-sector-38-noida%2Freviews%3Famp%3D1&psig=AOvVaw2ihqJFhgqDTLC3ujvRMBmo&ust=1723990658540000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMCyou-b_IcDFQAAAAAdAAAAABAE",
+           isveg : true
+        },
+        {
+           title : "Mutton Handi",
+           description :"“A pot full of flavor: Mutton Handi magic! 🍲”",
+           price : "₹ 850",
+           imgUrl:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dhandi%2Bmutton&psig=AOvVaw2_dOGKX6wF0BKXkOD3cN0j&ust=1723990709206000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJj_uIGc_IcDFQAAAAAdAAAAABAE",
+           isveg : false
+        },
+        {
+           title : "Burgur",
+           description :"“A classic combo: Burgur and love! ❤️”",
+           price : "₹ 120",
+           imgUrl:"https://www.google.com/url?sa=i&url=https%3A%2/F%2Fwww.pinterest.com%2Fpin%2F551111111111111&psig=AOvVaw3x6zQ8Q6Q4Q&ust=1724224719508000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwjy5c_gg4OIAxUPTWwGHUVqC78QjRx6BAgAEB",
+           isveg : true
+     
+        },
+        {
+              title : "Burgur",
+              description :"“A classic combo: Burgur and love! ❤️”",
+              price : "₹ 120",
+              imgUrl:"https://www.google.com/url?sa=i&url=https%3A%2/F%2Fwww.pinterest.com%2Fpin%2F551111111111111&psig=AOvVaw3x6zQ8Q6Q4Q&ust=1724224719508000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwjy5c_gg4OIAxUPTWwGHUVqC78QjRx6BAgAEB",
+              isveg : true
+        
+         },
+         {
+           title : "Mutton Handi",
+           description :"“A pot full of flavor: Mutton Handi magic! 🍲”",
+           price : "₹ 850",
+           imgUrl:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dhandi%2Bmutton&psig=AOvVaw2_dOGKX6wF0BKXkOD3cN0j&ust=1723990709206000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJj_uIGc_IcDFQAAAAAdAAAAABAE",
+           isveg : false
+        },
+        {
+           title : "Mutton Handi",
+           description :"“A pot full of flavor: Mutton Handi magic! 🍲”",
+           price : "₹ 850",
+           imgUrl:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dhandi%2Bmutton&psig=AOvVaw2_dOGKX6wF0BKXkOD3cN0j&ust=1723990709206000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJj_uIGc_IcDFQAAAAAdAAAAABAE",
+           isveg : false
+        },
+        
+]
+
+
+const THEME = 
+ {
+    main : "#fffffe",
+    highlight : "#3da9fc",
+    secondary : "#90b4ce",
+    tartiary : "#ef4565"
+ }
+
+
+export {
+    PRODUCTS,
+    THEME,
+    MAIN_TITLE
+}
